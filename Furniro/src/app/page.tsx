e954e36218f19622feed9e9d8e792baab05a4092
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2025-01-22 17:34:06 +0500

    Update page.tsx

--- Furniro/src/app/page.tsx
+++ Furniro/src/app/page.tsx
@@ -1,13 +1,32 @@
 
-import React from 'react'
-import Home from './Home/page'
-export default async function page() {
+import React from 'react';
+import Image from 'next/image';
+import Range from '@/components/Range';
+import Products from '@/components/Products';
+import Rooms from '@/components/Rooms';
+import Furniture from '@/components/Furniture';
+
+
+const Home = () => {
   return (
-    <div>
-      <Home/>
+    <div className="mt-[70px]">
+      <div className="relative">
+        <Image
+          src="/banner.jpg"
+          alt="Banner"
+          width={1024}
+          height={350}
+          className="w-full h-auto object-cover"
+        />
+       
+      </div>
+
+      <Range />
+      <Products />
+      <Rooms />
+      <Furniture />
     </div>
-  )
-}
+  );
+};
 
-
-
+export default Home;
